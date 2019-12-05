e1c305a9bc7dcce69aeb46969ee1219959eeafcb
심규민 <simgyumin@simgyumin-ui-MacBookPro.local>
2019-12-05 17:37:39 +0900

    JsCanvas mode : stroke, brush size control, fill mode change control

--- app.js
+++ app.js
@@ -1,20 +1,74 @@
 const canvas = document.getElementById("jsCanvas");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+
+const ctx = canvas.getContext("2d");
+
+canvas.width = 700;
+canvas.height = 700;
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
-function onMouseMove(event) {
-  let x = event.offsetX;
-  let y = event.offsetY;
+function stopPainting() {
+  painting = false;
 }
 
-function onMouseDown(event) {
-  let x = event.offsetX;
-  let y = event.offsetY;
+function startPainting() {
+  painting = true;
+}
 
-  console.log(x, y);
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+
+function handleColorClick(event) {
+  ctx.strokeStyle = event.target.style.backgroundColor;
+}
+
+function handleRangeControl(event) {
+  const size = event.target.value;
+  console.log(size);
+  ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+  if (filling === true) {
+    filling = false;
+    mode.innerText = "FILL";
+  } else {
+    filling = true;
+    mode.innerText = "BRUSH";
+  }
 }
 
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
+
+Array.from(colors).forEach(color =>
+  color.addEventListener("click", handleColorClick)
+);
+
+if (range) {
+  range.addEventListener("input", handleRangeControl);
+}
+
+if (mode) {
+  mode.addEventListener("click", handleModeClick);
 }
